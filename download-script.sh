6af98604782b27dd3b5cffa87ec28460216c2739
DorYehoshua <dor@sosiv.io>
2023-07-25 17:02:45 +0300

    release-1.6.1

--- download-script.sh
+++ download-script.sh
@@ -12,7 +12,7 @@ declare -a microServices
 
 # dev-collector sosiviodb dash as testing. 
 
-version="release-1.6.0"
+version="release-1.6.1"
 microServices=("analyzer" "authentication" "actuator" "classifier" "classifier-container-logs" "crud-manager" "contract-testing" "communicator" "sosivio-node-collector" "sosivio-node-pinger" "correlation-entities" "nsq" "sosivio-dashboard" "sequence-recognition" "discovery-engine" "sosiviodb" "notifier" "network-aggregator" "prediction-engine" )
 ourDockerRepo="release.sosiv.io"
 userName="customer"           # Client default user
